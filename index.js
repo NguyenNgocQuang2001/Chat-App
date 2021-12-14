6aab96401399e4a78677a862f2f27f1d482d0dd8
NguyenNgocQuang2001 <sinhvienuetk64@gmail.com>
2021-12-15 00:55:52 +0700

    commit 2021-12-15 00:56

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server} = require('socket.io');
-
+const PORT = process.env.PORT || 3000;
 const io = new Server(server);
 
 app.get('/', (req, res, next) => {
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
         io.emit('user-chat', data);
     })
 })
-server.listen(3000, () => {
+server.listen(PORT, () => {
 
-    console.log('server is running on port 3000');
+    console.log(`server is running on port ${PORT}`);
 })
